bac0f3455079a0e3fa20c1f41e743d51a2f2c65d
Boris Zhao <boris1993@126.com>
2021-01-05 21:31:53 +0800

    Refactor shell scripts

--- resources/start.sh
+++ resources/start.sh
@@ -9,16 +9,16 @@ UPNP_LOG_FILE="/var/log/upnp.log"
 echo "Starting the cron" >> "$LOG_FILE"
 /etc/init.d/cron start
 
-# Sleep for 2 minutes in order to wait for the program 
-# finishes self-updating
-echo "Sleeping for 2 minutes" >> "$LOG_FILE"
-sleep 120
-
 # Then wait for the program running
 echo "Waiting for the program be running" >> "$LOG_FILE"
 while ! pgrep ${PROCESS_NAME}; do
   sleep 30
 done
+
+# Sleep for 2 minutes in order to wait for the program 
+# finishes self-updating
+echo "Sleeping for 30 seconds in order to let the app fully initialized" >> "$LOG_FILE"
+sleep 30
 
 # Then initialize the UPnP
 echo "Initializing UPnP port forwarding in the background" >> "$LOG_FILE"
