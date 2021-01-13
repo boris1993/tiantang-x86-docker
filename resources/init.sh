258c1ed055f6c8a9a5259b9c55e3169af4f0f7fa
Boris Zhao <boris1993@126.com>
2021-01-13 23:37:43 +0800

    Refactor

--- resources/init.sh
+++ resources/init.sh
@@ -4,6 +4,9 @@ PROCESS_NAME="ttnode"
 LOG_FILE="/var/log/app.log"
 UPNP_LOG_FILE="/var/log/upnp.log"
 
+# Sleep for 30 seconds in order to let the app finishes the self-update
+sleep 30
+
 # Wait for the program running
 echo "Waiting for the program be running" >> "$LOG_FILE"
 while ! pgrep ${PROCESS_NAME}; do
@@ -11,8 +14,8 @@ while ! pgrep ${PROCESS_NAME}; do
 done
 
 # Sleep for another 30 seconds in order to wait for the program finishes initializing
-echo "Sleeping for 30 seconds in order to let the app fully initialized" >> "$LOG_FILE"
-sleep 30
+echo "Sleeping for 60 seconds in order to let the app fully initialized" >> "$LOG_FILE"
+sleep 60
 
 # Then initialize the UPnP
 echo "Initializing UPnP port forwarding in the background" >> "$LOG_FILE"
--- resources/start.sh
+++ resources/start.sh
@@ -7,6 +7,8 @@ LOG_FILE="/var/log/app.log"
 echo "Starting the cron" >> "$LOG_FILE"
 /etc/init.d/cron start
 
+liveness-check.sh &
+
 init.sh &
 
 tail -f /var/log/app.log
