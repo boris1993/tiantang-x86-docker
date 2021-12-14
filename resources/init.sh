fb691ab2fe73715237ac8b8e2366705714be5ab0
Boris Zhao <boris1993@live.cn>
2021-12-15 00:38:11 +0800

    Remove the script for setting up UPnP
    As the ttnode itself will handle that when using the host network

--- resources/init.sh
+++ resources/init.sh
@@ -13,12 +13,3 @@ while ! pgrep "${PROCESS_NAME}"; do
 done
 
 print-qrcode.sh >> "${LOG_FILE}"
-
-# SLEEP_SECONDS=90
-# Sleep for a while in order to wait for the program finishes initializing
-# echo "[$TIME] Sleeping for ${SLEEP_SECONDS} seconds in order to let the app fully initialized" >> "$LOG_FILE"
-# sleep ${SLEEP_SECONDS}
-
-# Then initialize the UPnP
-echo "[$(print_time)] Initializing UPnP port forwarding in the background" >> "$LOG_FILE"
-(set-port-forwarding.sh | tee -a "$UPNP_LOG_FILE" "$LOG_FILE" 2>&1 &) > /dev/null
